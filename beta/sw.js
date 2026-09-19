0f5038dbae09178e1568b605abf8f138d8c3b8c8
reisnerfrantisek-a11y <reisnerfrantisek@gmail.com>
2026-09-19 19:38:23 +0200

    Bump beta cache to 1.5.11

--- beta/sw.js
+++ beta/sw.js
@@ -1,8 +1,8 @@
 const SCOPE_TAG=new URL(self.registration.scope).pathname.includes('/beta/')?'beta':'prod';
 const CACHE_PREFIX=`energo-prehled-${SCOPE_TAG}-`;
-const CACHE=`${CACHE_PREFIX}v1.5.10`;
+const CACHE=`${CACHE_PREFIX}v1.5.11`;
 const LEGACY_CACHES=SCOPE_TAG==='beta'?['energo-prehled-v1.1.0']:['energo-prehled-v1.0.0'];
-const ASSETS=['./','./index.html','./styles.css?v=1.5.10','./app.js?v=1.5.10','./manifest.webmanifest','../icons/icon-180.png','../icons/icon-192.png','../icons/icon-512.png'];
+const ASSETS=['./','./index.html','./styles.css?v=1.5.11','./app.js?v=1.5.11','./manifest.webmanifest','../icons/icon-180.png','../icons/icon-192.png','../icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>(k.startsWith(CACHE_PREFIX)&&k!==CACHE)||LEGACY_CACHES.includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
